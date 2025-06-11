8ee4fe39b4c859e96263b0a06fdab1aca394b153
Luqman Hadi <102597758+luqhardy@users.noreply.github.com>
2025-06-11 09:58:48 +0900

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import { createApp } from 'vue';
-import ProfileCard from './Components/ProfileCard.vue'; // Adjust path if needed
+import ProfileCard from './components/ProfileCard.vue'; // パスを修正
 
 const app = createApp({});
 app.component('ProfileCard', ProfileCard);
